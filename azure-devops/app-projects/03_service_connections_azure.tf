604f14fa9a9f2f5fc409f2543463bc407cc8549b
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-05-06 16:44:27 +0200

    changed secret module, to secret_azdo

--- azure-devops/app-projects/03_service_connections_azure.tf
+++ azure-devops/app-projects/03_service_connections_azure.tf
@@ -2,30 +2,6 @@
 # ⛩ Service connections
 #
 
-# 🟢 DEV service connection
-resource "azuredevops_serviceendpoint_azurerm" "DEV-SERVICE-CONN" {
-  depends_on = [azuredevops_project.project]
-
-  project_id                = azuredevops_project.project.id
-  service_endpoint_name     = "${var.dev_subscription_name}-SERVICE-CONN"
-  description               = "${var.dev_subscription_name} Service connection"
-  azurerm_subscription_name = var.dev_subscription_name
-  azurerm_spn_tenantid      = module.secrets.values["TENANTID"].value
-  azurerm_subscription_id   = module.secrets.values["DEV-SUBSCRIPTION-ID"].value
-}
-
-# 🟨 UAT service connection
-resource "azuredevops_serviceendpoint_azurerm" "UAT-SERVICE-CONN" {
-  depends_on = [azuredevops_project.project]
-
-  project_id                = azuredevops_project.project.id
-  service_endpoint_name     = "${var.uat_subscription_name}-SERVICE-CONN"
-  description               = "${var.uat_subscription_name} Service connection"
-  azurerm_subscription_name = var.uat_subscription_name
-  azurerm_spn_tenantid      = module.secrets.values["TENANTID"].value
-  azurerm_subscription_id   = module.secrets.values["UAT-SUBSCRIPTION-ID"].value
-}
-
 # 🛑 PROD service connection
 resource "azuredevops_serviceendpoint_azurerm" "PROD-SERVICE-CONN" {
   depends_on = [azuredevops_project.project]
@@ -34,6 +10,6 @@ resource "azuredevops_serviceendpoint_azurerm" "PROD-SERVICE-CONN" {
   service_endpoint_name     = "${var.prod_subscription_name}-SERVICE-CONN"
   description               = "${var.prod_subscription_name} Service connection"
   azurerm_subscription_name = var.prod_subscription_name
-  azurerm_spn_tenantid      = module.secrets.values["TENANTID"].value
-  azurerm_subscription_id   = module.secrets.values["PROD-SUBSCRIPTION-ID"].value
+  azurerm_spn_tenantid      = module.secret_azdos.values["TENANTID"].value
+  azurerm_subscription_id   = module.secret_azdos.values["PROD-SUBSCRIPTION-ID"].value
 }
